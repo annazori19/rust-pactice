30bb87f46cc571ed78a042c538608decfa701afc
Anna <anna.zorina@e-u.edu.ua>
2026-04-25 04:37:32 +0300

    completed task 09: migratory birds

--- src/hackerrank/task09.rs
+++ src/hackerrank/task09.rs
@@ -1,1 +1,30 @@
-// https://www.hackerrank.com/challenges/migratory-birds/problem
+pub fn migratory_birds(arr: Vec<i32>) -> i32 {
+    let mut counts = vec![0; 6];
+
+    for &bird_id in arr.iter() {
+        counts[bird_id as usize] += 1;
+    }
+
+    let mut max_count = 0;
+    let mut result_id = 0;
+
+    for i in 1..=5 {
+        if counts[i] > max_count {
+            max_count = counts[i];
+            result_id = i as i32;
+        }
+    }
+
+    result_id
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_migratory_birds() {
+        assert_eq!(migratory_birds(vec![1, 4, 4, 4, 5, 3]), 4);
+        assert_eq!(migratory_birds(vec![1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]), 3);
+    }
+}
